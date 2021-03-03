791d6fa85c95220636c6cdeff94f7e9ff320ec94
ManongJu <469863752@qq.com>
2021-03-03 18:05:16 +0800

    sentinel 熔断和限流

--- consumer/src/main/java/com/example/consumer/demo/controller/ConsumerController.java
+++ consumer/src/main/java/com/example/consumer/demo/controller/ConsumerController.java
@@ -4,7 +4,6 @@ import com.example.consumer.demo.facade.ProviderFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ConsumerController {
